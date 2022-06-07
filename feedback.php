4c99cb36c6841309fea0c4b01d2d43a88e44f310
janakarthi <m.s.m.janarthanan@gmail.com>
2022-06-07 12:11:26 +0530

    time_manage

---
+++ feedback.php
@@ -0,0 +1,51 @@
+<?php 
+ob_start(); 
+require 'config.php';
+
+$view = $_POST['view'];
+$name = $_POST['name'];
+$comments = $_POST['comments'];
+$manager = $_POST['manager'];
+$Smanager = $_POST['Smanager'];
+$name_value = $_POST['name_value'];
+
+
+$query = mysqli_query($con, "INSERT INTO `poll`(`id`, `name`, `feedback`, `suggestions`,`manager`,`Smanager`,`name_value`) VALUES ('','$name','$view','$comments','$manager', '$Smanager', '$name_value')");
+echo '<script>alert("Thank You..! Your Feedback is Valuable to Us"); location.replace(document.referrer);</script>';
+
+
+
+/*$query1 = "SELECT * FROM `staff` WHERE email = '$email' AND password = '$password'";
+$result_query1 = mysqli_query($conn, $query1);
+$count_query1 = mysqli_num_rows($result_query1);
+/*if ($count_query1 != 0) 
+	{
+    header("Location: controller/staff.php");
+	exit();
+	} 
+else {
+		    $query2 = "SELECT * FROM `admin` WHERE email = '$email' AND password = '$password'";
+			$result_query2 = mysqli_query($conn, $query2);
+			$count_query2 = mysqli_num_rows($result_query2);
+			$count_query2 = mysqli_num_rows($result_query2);
+			if ($count_query2!=0) 
+			{
+		   		header("Location: controller/admin.php");
+				exit();
+		   	} 		
+		   	else
+		   	 {
+		   	 	echo '<script>alert("Incorrect Credentials Entered"); location.replace(document.referrer);</script>';
+
+			 }
+
+	}
+
+//echo $email;
+//echo '</br>';
+//echo $password;
+
+
+
+ob_end_flush();*/
+?>
---
+++ index.php
@@ -0,0 +1,79 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Nexus</title>
+<!-- custom-theme -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Elegant Feedback Form  Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //custom-theme -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="//fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+</head>
+<body class="agileits_w3layouts">
+    <h1 style="color:#11F0BE;"class="agile_head text-center" >Quarterly Feedback Form</h1>
+    <div class="w3layouts_main wrap">
+	 
+	    <form action="feedback.php" method="post" class="agile_form">
+	    	
+	
+		  <h2>Select Your Location</h2>
+			 <ul class="agile_info_select">
+				 <li><input type="radio" name="view" value="Chennai" id="excellent" required> 
+				 	  <label for="excellent">Chennai</label>
+				      <div class="check w3"></div>
+				 </li>
+				 <li><input type="radio" name="view" value="Pondy" id="good"> 
+					  <label for="good"> Pondy</label>
+				      <div class="check w3ls"></div>
+				 </li>
+				 <li><input type="radio" name="view" value="Tenkasi" id="neutral">
+					 <label for="neutral">Tenkasi</label>
+				     <div class="check wthree"></div>
+				 </li>
+				</ul>
+			<h2>Select Your Manager</h2>
+			 <ul class="agile_info_select">
+				 <li><input type="radio" name="Smanager" value="Raghav" id="excellent1" required> 
+				 	  <label for="excellent1">Raghav</label>
+				      <div class="check w3"></div>
+				 </li>
+				 <li><input type="radio" name="Smanager" value="Babu" id="good1"> 
+					  <label for="good1"> Babu</label>
+				      <div class="check w3ls"></div>
+				 </li>
+				 <li><input type="radio" name="Smanager" value="Jerry" id="neutral1">
+					 <label for="neutral1">Jerry</label>
+				     <div class="check wthree"></div>
+				 </li>
+				  <li><input type="radio" name="Smanager" value="Jayalakshmi" id="neutrall">
+					 <label for="neutrall">Jayalakshmi</label>
+				     <div class="check wthree"></div>
+				 </li>
+				</ul>
+				<br>
+				<select name="name_value" id="name_value">
+        <option value="ugesh">ugesh</option>
+        <option value="Ruban">Ruban</option>
+        <option value="test">test</option>
+        <option value="jana">jana</option>
+    </select>
+			<h1 style="color:orange;">Feedback about Manager </h1>
+			<h2>Do you feel like your manager is had a good level of transparency with you and your team?</h2>
+			<textarea placeholder="Manager" class="w3l_summary" name="manager" ></textarea>	  
+			<h2>If you have specific feedback, please write to us...</h2>
+			<textarea placeholder="Additional comments" class="w3l_summary" name="comments" ></textarea>
+			
+			<input type="text" placeholder="Your Name (optional)" name="name"  />
+			<br>
+			<center><input type="submit" value="submit Feedback" class="agileinfo" /></center>
+	  </form>
+	</div>
+	<div class="agileits_copyright text-center">
+			<p style="color:#11F0BE;">© 2022 NEXUS</p>
+	</div>
+</body>
+</html>
